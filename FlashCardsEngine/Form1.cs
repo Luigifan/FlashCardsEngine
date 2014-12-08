9992c375c373be83d3b95d5c17452ace4a89962c
Mike Santiago <miketheripper1@msn.com>
2014-12-08 16:37:54 -0500

    Initial commit

---
+++ FlashCardsEngine/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using IniParser.Parser;
+using IniParser;
+using IniParser.Model;
+
+namespace FlashCardsEngine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /*var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile("");
+            foreach (var i in data.Sections["SpecialChar"])
+            {
+                
+            }*/
+        }
+    }
+}
