2bf58dc4a30a61d049066d4f4bbcee8cc48a64fa
Mike Santiago <miketheripper1@msn.com>
2014-12-13 18:10:03 -0500

    started some more class work

---
+++ FlashCardsEngine/Core/UnstricAnswer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlashCardsEngine.Core
+{
+    public class UnstricAnswer
+    {
+        public string[] PossibleAnswers(string strictAnswer)
+        {
+            string[] split = strictAnswer.Split(' ');
+            List<string> matches = new List<string>();
+            foreach (var i in split)
+            {
+                if(i.StartsWith("{") && i.EndsWith("}"))
+                {
+                    matches.Add(i.Trim('{', '}'));
+                }
+            }
+            return matches.ToArray();
+        }
+        //
+        public string StrippedAnswer(string answer)
+        {
+            return answer.Trim('{', '}');
+        }
+        //
+    }
+}
